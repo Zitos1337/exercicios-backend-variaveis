3655c4c994dfbed2c94f9547e083ce08b93f7709
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-21 14:49:48 -0300

    Update index.js

--- exercicios-backend-variaveis-master/08/index.js
+++ exercicios-backend-variaveis-master/08/index.js
@@ -1,8 +1,7 @@
-var R = 1;
-var pi = 3.14159;
+const pi = 3.14
+const r = 1;
+const c = 2 * pi * r;
+const a = pi * r * r;
 
-var C = 2 * pi * R;
-var A = pi * R * R;
-
-console.log("Circunferência (C): " + C.toFixed(2));
-console.log("Área (A): " + A.toFixed(2));
+console.log(c);
+console.log(a);
