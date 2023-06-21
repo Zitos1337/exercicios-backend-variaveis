04d83c6fdece72a82263c52f698ff5f271222abd
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-21 14:23:21 -0300

    Update index.js

--- exercicios-backend-variaveis-master/06/index.js
+++ exercicios-backend-variaveis-master/06/index.js
@@ -1,10 +1,7 @@
-function calcularVelocidade(distPercorrida, tempo) {
-    const velocidadeKmPorHora = (distPercorrida / tempo) * 3.6;
-    return velocidadeKmPorHora;
-}
-
-const distPercorrida = 500;
+const distpercorrida = 500;
 const tempo = 10;
+const velocidade = distpercorrida / tempo;
+console.log(`${velocidade}m/s`);
 
-const velocidade = calcularVelocidade(distPercorrida, tempo);
-console.log("A velocidade é de " + velocidade + " km/h");
+const velocidadeEmKm = velocidade * 3.6;
+console.log(`${velocidadeEmKm}km/h`);
