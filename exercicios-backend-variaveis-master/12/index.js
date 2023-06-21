2dd41145f053a9d2f994f382299137ace0215e71
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-21 13:04:40 -0300

    Update index.js

--- exercicios-backend-variaveis-master/12/index.js
+++ exercicios-backend-variaveis-master/12/index.js
@@ -1,9 +1,6 @@
-var M = 90000;
-var C = 60000;
-var n = 24;
+const capitalInicial = 60000;
+const montante = 90000;
+const numeroDeMeses = 24;
+const taxaDeJuros = Math.pow(montante / capitalInicial, 1 / numeroDeMeses) - 1
 
-var i = Math.pow(M / C, 1 / n) - 1;
-
-var taxaJurosPercentual = i * 100;
-
-console.log("Taxa de juros mensal: " + taxaJurosPercentual.toFixed(2) + "%");
+console.log(`O seu financiamento de ${capitalInicial} reais teve uma taxa de juros de ${taxaDeJuros}% pois após 24 meses você teve que pagar 90000 reais`);
